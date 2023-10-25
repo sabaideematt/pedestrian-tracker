ebae1cb5a2fae0a291323a37b48613bacb3fa06b
Matthew Cabrera <coolegoodes@gmail.com>
2023-10-25 00:49:49 +0000

    app file

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# Your pedestrian tracking function that generates frames
+from pedestrian_tracking import generate_frames
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate():
+    # Invoke your function that produces frames
+    for frame in generate_frames():
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
