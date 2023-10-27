eb1250ba9a046876d882cc12ee7103049ddf0e0d
Matthew Cabrera <coolegoodes@gmail.com>
2023-10-27 01:55:18 +0000

    sqllite addition for statistics

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, url_for
+import cv2
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Your pedestrian tracking function that generates frames
-from pedestrian_tracking import generate_frames
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pedestrian_data.db'
+db = SQLAlchemy(app)
+
+# Database Model
+class PedestrianData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    count = db.Column(db.Integer)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def generate():
-    # Invoke your function that produces frames
-    for frame in generate_frames():
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def generate_frames():
+    camera = cv2.VideoCapture('path_to_video.mp4')
+    while True:
+        success, frame = camera.read()  
+        if not success:
+            break
+        else:
+            # Convert the image frame into a jpeg format and then return it
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            
+            # (You can integrate the pedestrian detection logic here and update the database accordingly.)
+            
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    camera.release()
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    db.create_all()  # This will create the pedestrian_data.db file and the PedestrianData table
+    app.run(debug=True)
--- pedestrian-tracker.py
+++ pedestrian-tracker.py
@@ -1,35 +1,57 @@
 import cv2
+from flask import Flask, render_template, Response, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pedestrian_data.db'
+db = SQLAlchemy(app)
+
+# Database Model
+class PedestrianData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    count = db.Column(db.Integer)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 def generate_frames():
-    # Initialize video capture
-    cap = cv2.VideoCapture("path/to/your/video.mp4")
-
-    # Initialize pedestrian detector
+    camera = cv2.VideoCapture('path_to_video.mp4')
     pedestrian_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
 
     while True:
-        ret, frame = cap.read()
-        
-        if not ret:
+        success, frame = camera.read()  
+        if not success:
             break
+        else:
+            # Pedestrian detection
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            pedestrians = pedestrian_cascade.detectMultiScale(gray, 1.3, 2)
+            
+            for (x,y,w,h) in pedestrians:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        # Convert to grayscale
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # Detect pedestrians
-        pedestrians = pedestrian_cascade.detectMultiScale(gray, 1.1, 3)
-        
-        # Draw bounding box
-        for (x, y, w, h) in pedestrians:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        
-        # Encode the frame as JPEG
-        ret, buffer = cv2.imencode('.jpg', frame)
-        
-        # Convert to bytes
-        frame = buffer.tobytes()
-        
-        yield frame
+            # Update the database with the count of detected pedestrians
+            new_data = PedestrianData(count=len(pedestrians))
+            db.session.add(new_data)
+            db.session.commit()
 
-    # Release the video capture object
-    cap.release()
+            # Convert the image frame into a jpeg format and then return it
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            
+    camera.release()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    db.create_all()  # This will create the pedestrian_data.db file and the PedestrianData table
+    app.run(debug=True)
